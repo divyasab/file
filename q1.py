b372dcc5dd37a6bce665173260b93d50ff2bebb3
Divya sable <103986474+divyasab@users.noreply.github.com>
2022-12-16 15:41:37 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,26 @@
+# a=[]
+# for i in range(3):
+#     name=(input("enter the name"))
+#     a.append(name)
+# for i in a:
+#     print(i)
+
+# #
+
+# a=int(input("enter the number"))
+
+# if a<-5:
+#     print(-a)
+# elif a<5:
+#      print(-a)
+
+
+
+
+# num=int(input("enter the number"))
+# if num>0:
+#         print("positive","+",5)
+# elif num<0:
+#         print("negative number","-",5)
+# else:
+#     print("zero")
---
+++ q2.py
@@ -0,0 +1,96 @@
+
+
+
+
+
+# create file
+
+# from turtle import onscreenclick
+
+
+# f=open("file1.txt","x")
+
+# write file
+# f=open("file1.txt","w")
+# f.write("vidya")
+# f.close()
+
+# read file
+# f=open("file1","r")
+# print(f.read())
+# f.close()
+
+# # write file
+# f=open("file1","w")
+# f.write("my name is")
+# f.close()
+
+
+# f=open("vidya","x")
+
+# f=open("vidya","w")
+# f.write("she is my sister")
+# f.close()
+
+# f=open("vidya","r")
+# print(f.read())
+# f.close()
+
+# append file
+# f=open("vidya","a")
+# f.write("divya")
+# f.close() 
+
+# read file
+# f=open("vidya","r")
+# print(f.read())
+# f.close()
+
+# f=open("vidya","w")
+# f.write("divya sable my ame is")
+# f.close()
+
+# append word count
+# f=open("vidya","a")
+# a=f.write("divya")
+# print(a)
+# f.close()
+
+# w+ file
+# f=open("vidya","w+")
+# print(f.read())
+# f.close()
+
+# # readine
+# f = open("vidya", "r")
+# print(f.readline())
+
+
+# f=open("file1.txt","r")
+# print(f.read())
+# f.close()
+
+
+# f=open("file1.txt","w")
+# f.write("divya")
+# f.close()
+
+# f=open("file1.txt","a")
+# f.write("vidya")
+# f.close()
+
+# a=open("simi.txt","x")
+
+
+# a=open("simi.txt","w")
+# a.write("divya")
+# a.close()
+
+# a=open("simi.txt","r")
+# print(a.read())
+# a.close()
+
+# a=open("simi.txt","a")
+# a.write("simi")
+# a.close()
+
